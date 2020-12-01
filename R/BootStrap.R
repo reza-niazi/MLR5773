8712baf9c04c1e50c8f58455e1e37ec9665dd858
Reza Niazi <rniazi@math.ou.edu>
2020-12-01 15:36:37 -0600

    1

---
+++ R/BootStrap.R
@@ -0,0 +1,67 @@
+#' Bootstrap to Determine Regression Coefficients
+#'
+#' @param iter the number of iterations to perform in bootstrap
+#' @param alpha the significance level for confidence intervals
+#' @param X the design matrix
+#' @param Y the response vector
+#'
+#' @return regression coefficients and histogram of regression coefficients vs iterations along with comparison to classical regression approach
+#' @export
+#'
+#' @examples Bootstrap(1000,.05,X,Y) where X and Y are determined by data OR Bootstrap(100000,.1,X,Y)
+Bootstrap <- function(iter,alpha,X,Y){
+  colnames(X) = c()
+  rownames(X) = c()
+  ncol = ncol(X)
+  nrow=nrow(X)
+  beta_matrix = matrix(nrow=iter,ncol=ncol)
+  for(k in 1:ncol){
+    beta_matrix[,k] = numeric(iter)
+  }
+  for(i in 1:iter){
+    indices = sample(x = 1:nrow, size = nrow, replace = TRUE)
+    newX = X[indices,]
+    beta_hat = try(solve(t(newX)%*%newX)%*%t(newX)%*%Y,silent=TRUE)
+
+    if(inherits(beta_hat,"try-error")){
+      counter = counter+1
+    }
+    else{
+      for(k in 1:ncol){
+        beta_matrix[i,k] = beta_hat[k,]
+      }
+    }
+  }
+  for(k in 1:ncol){
+    hist(x = beta_matrix[,k], probability = TRUE,
+         main = paste("Bootstrapped beta",(k-1)),
+         xlab = paste("beta",(k-1)," estimates"),col="blue")
+  }
+
+  for(k in 1:ncol){
+    beta_ave = sum(beta_matrix[,k])/iter
+    print(paste("Point Estimate from bootstrap for beta ", (k-1)))
+    print(beta_ave)
+    print(paste("CI from bootstrap for beta ", (k-1)))
+    print(beta_ave*c(1,1)+c(-1,1)*qnorm(1-alpha/2)*sqrt(nrow/(nrow-1))*sqrt(sum((beta_matrix[,k]-beta_ave)**2)/(iter-1)))
+  }
+
+
+  beta_hat = try(solve(t(X)%*%X)%*%t(X)%*%Y,silent=TRUE)
+
+  if(inherits(beta_hat,"try-error")){
+    print("A singularity occured when trying to compute the regression coefficients")
+  }
+  else{
+    print("Regression Coefficeints (starting at beta0) from classical ML formulas:")
+    print(beta_hat)
+    sse = t(Y)%*%Y-t(beta_hat)%*%t(X)%*%Y
+    s = sqrt(sse/(nrow-ncol))
+    C = solve(t(X)%*%X)
+    for(k in 1:ncol)
+    {
+      print("Confidence interval for regression coefficients from classical ML formulas:")
+      print(beta_hat[k]*c(1,1)+c(-1,1)*c(qt(1-alpha/2,(nrow-ncol))*s*sqrt(C[k,k])))
+    }
+  }
+}
---
+++ R/LinearModel.R
@@ -0,0 +1,34 @@
+LinearModel <- function(X,Y){
+  colnames(X) = c()
+  rownames(X) = c()
+  colnames(Y) = c()
+  rownames(Y) = c()
+  C = solve(t(X)%*%X)
+  beta_hat = C%*%t(X)%*%Y
+  n=dim(X)[1]
+  k = dim(X)[2]-1
+  print(c("Regression Coefficients Starting at beta_0:",beta_hat))
+
+  SSE = t(Y)%*%Y-t(beta_hat)%*%t(X)%*%Y
+  s2=SSE/(n-(k+1))
+  s = sqrt(s2)
+  print(c("SSE:",SSE,"Residual Standard Error:",s))
+
+  ttest_vals = numeric(k+1)
+  for(i in 1:(k+1)){
+    ttest_vals[i]=beta_hat[i]/(s*sqrt(C[i,i]))
+  }
+  ttest_pvals = (1-pt(abs(ttest_vals),(n-(k+1))))*2
+  print(c("T Statistic for each beta term starting at beta0:",ttest_vals))
+  print(c("p value for each T test starting at beta0:",ttest_pvals))
+
+  SSyy = t(Y)%*%Y-((sum(Y))^2)/n
+  R2 = R2 = 1-SSE/SSyy
+  R2a  = 1-((n-1)/(n-(k+1)))*(1-R2)
+  print(c("R Squared Value",R2,"Adjusted R squared Value",R2a))
+
+  Fstat = (R2/k)/((1-R2)/(n-(k+1)))
+  print(Fstat)
+  F_pval = 1-pf(Fstat,k,(n-(k+1)))
+  print(c("F Statistic Value:",Fstat,"F Test p value",F_pval))
+}
