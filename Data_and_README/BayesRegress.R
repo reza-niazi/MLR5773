e12ffc7d4e64d611da320d1cf7c65a48000a8f26
Reza Niazi <rniazi@math.ou.edu>
2020-12-03 22:28:15 -0600

    11

--- Data_and_README/BayesRegress.R
+++ Data_and_README/BayesRegress.R
@@ -1,4 +1,7 @@
-#' Bayesian Regression
+#' @title Regression through the Markov Chain Monte Carlo Method
+#'
+#' MCMC is used in order to create point and intervals estimates of regression coefficients. Along with these, diagnostic plots like posterior densities,
+#' are presented to show that the process converged. The estimates are compared to the classical multilinear regression point and interval estimates.
 #'
 #' @param formula the formula of the model that is desired to be fit
 #' @param data the data used for fitting
--- Data_and_README/ModelCheck.R
+++ Data_and_README/ModelCheck.R
@@ -1,11 +1,18 @@
-#' Checking Regression Asummptions
+#' @title Checking Regression Assumptions
 #'
-#' @param lm a linear model object
+#' @param lm: a linear model object
+#'
+#' This function checks whether a given linear model satisfies the regression assumptions. That is whether the errors are iid from a N(0,sigma^2*I)
+#' distribution. The check on a mean of 0 and constant variance is done by making a residual vs fitted plot. The check on normality is done
+#' by making a QQ plot and also running the Shapiro-Wilk Test. The independence of the errors is guaranteed for non time-series data, which is
+#' all we will consider. Therefore, the function does not explicitely adress this.
+#'
+#'
 #'
 #' @return Commandline Statistics and Diagnostic Plots to determine if regression assumptions are met
 #' @export
 #'
-#' @examples lm = lm(y~x,data) ---- ModelCheck(lm) or lm = lm(y~x1+x2,data) ----- ModelChec(lm)
+#' @examples lm = lm(y~x,data) ----> ModelCheck(lm) or lm = lm(y~x1+x2,data) -----> ModelCheck(lm)
 
 ModelCheck <- function(lm){
   library(s20x)
